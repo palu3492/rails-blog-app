f30386101dc694fa95719a2895796442fdd1ad3c
Alex Palumbo <palu3492@stthomas.edu>
2020-07-30 09:56:24 -0500

    Added edit and delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,20 @@
 class PostsController < ApplicationController
+  # GET /posts
   def index
     @posts = Post.all
   end
 
+  # GET /posts/:id
   def show
     @post = Post.find(params[:id])
   end
 
+  # GET /posts/new
   def new
 
   end
 
+  # POST /posts
   def create
     # render plain: params[:post].inspect
     @post = Post.new(post_params)
@@ -20,6 +24,29 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH or PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5}
-  validates :body, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: {minimum: 1, maximum: 280}
+  validates :body, presence: true, length: {minimum: 1, maximum: 280}
 end
