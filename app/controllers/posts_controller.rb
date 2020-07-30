1a967fbb46dd102bab26ff6763bbaf29e0959939
Alex Palumbo <palu3492@stthomas.edu>
2020-07-29 19:04:26 -0500

    Added posting functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
 
   end
 
-  # post request?
   def create
-    render plain: params[:post].inspect
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
